1050b8083642d10f05358ee60aa753ca81f5debc
Emad180 <67835370+Emad180@users.noreply.github.com>
2021-12-27 23:31:00 +0200

    Update lifetime.js

--- js/lifetime.js
+++ js/lifetime.js
@@ -12,7 +12,7 @@ window.onload = function () {
         const file1 = file[0];
         let reader = new FileReader();
         reader.onload = (e) => {
-            if(file1.type = 'text/plain' || file1.type = 'text/csv'){
+            //if(file1.type == 'text/plain' || file1.type == 'text/csv'){
                 const file2 = e.target.result;
                 const lines = file2.split(/\r\n|\n/);
                 lines.forEach(element => {
@@ -51,9 +51,9 @@ window.onload = function () {
                     xyArray.push(xArray[i]*yArray[i]);
                 }
                 document.getElementById('lifetime_result').innerHTML = (xyArray.reduce(sum)/yArray.reduce(sum)).toFixed(4);
-            }else{
-                alert('Upload .txt or .csv file format');
-            }
+            //}else{
+               // alert('Upload .txt or .csv file format');
+           // }
         }
         reader.onerror = (error) => {
             alert("Failed to read file!\n\n" + reader.error);
