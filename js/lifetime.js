7373ec3f9f94a657b84b863c407536be7d4d2cf3
Emad180 <67835370+Emad180@users.noreply.github.com>
2021-12-28 01:44:14 +0200

    Update lifetime.js

--- js/lifetime.js
+++ js/lifetime.js
@@ -13,7 +13,7 @@ window.onload = function () {
         let reader = new FileReader();
         reader.onload = (e) => {
             let extension = ['text/plain', 'text/csv', 'text/xls', 'text/xlsx', 'text/uxd']
-            if(file1.type.includes(extension){
+            if(file1.type.includes(extension)){
                 const file2 = e.target.result;
                 const lines = file2.split(/\r\n|\n/);
                 lines.forEach(element => {
