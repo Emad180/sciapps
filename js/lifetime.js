f498c571b08b6ec73b600902a36728d92dd3ba7d
Emad180 <67835370+Emad180@users.noreply.github.com>
2021-12-28 16:08:03 +0200

    Update lifetime.js

--- js/lifetime.js
+++ js/lifetime.js
@@ -15,45 +15,45 @@ window.onload = function () {
         let fileExt = file1.name.substring(index);
         if(fileExt === '.txt' || fileExt === '.csv' || fileExt === '.uxd' || fileExt === '.xls' || fileExt === '.xlsx'){
             reader.onload = (e) => {
-            const file2 = e.target.result;
-            const lines = file2.split(/\r\n|\n/);
-            lines.forEach(element => {
-                if(/[^0-9,.\s\tEe+]/g.test(element)){
-                    //pass
-                }else if(element == ''){
-                    //pass;
-                }else{
-                    let rowTrimed = element.replace(/^\s+|\s+$/gm,'');
-                    if(/[,]|\s+/.test(rowTrimed)){
-                        linesArray.push(rowTrimed.replace(/[,]|\s+/, '    '));
+                const file2 = e.target.result;
+                const lines = file2.split(/\r\n|\n/);
+                lines.forEach(element => {
+                    if(/[^0-9,.\s\tEe+]/g.test(element)){
+                        //pass
+                    }else if(element == ''){
+                        //pass;
                     }else{
-                        if(showAlert){
-                            alert(`the delemiter must be ','/'Tab'`);
-                            showAlert = false;
+                        let rowTrimed = element.replace(/^\s+|\s+$/gm,'');
+                        if(/[,]|\s+/.test(rowTrimed)){
+                            linesArray.push(rowTrimed.replace(/[,]|\s+/, '    '));
+                        }else{
+                            if(showAlert){
+                                alert(`the delemiter must be ','/'Tab'`);
+                                showAlert = false;
+                            }
                         }
                     }
+                });
+
+                var str = linesArray.join('\n');
+
+                document.getElementById('xcolumn').value = str; 
+                let textArea = str.split('\n')
+                textArea.forEach(line => {
+                    let value = line.split('    ');
+                    let xAxis = Number(value[0]);
+                    let yAxis = Number(value[1].replace(/[^0-9.\s\tEe+]/, ''));
+                    xArray.push(xAxis);
+                    yArray.push(yAxis);
+                });
+                //Where the processing function added to process the user's read file
+                console.log(xArray, yArray);
+                dataProcessing(xArray, yArray);
+                for(let i = 0; i < yArray.length; i++){
+                    xyArray.push(xArray[i]*yArray[i]);
                 }
-            });
-                
-            var str = linesArray.join('\n');
-         
-            document.getElementById('xcolumn').value = str; 
-            let textArea = str.split('\n')
-            textArea.forEach(line => {
-                let value = line.split('    ');
-                let xAxis = Number(value[0]);
-                let yAxis = Number(value[1].replace(/[^0-9.\s\tEe+]/, ''));
-                xArray.push(xAxis);
-                yArray.push(yAxis);
-            });
-            //Where the processing function added to process the user's read file
-            console.log(xArray, yArray);
-            dataProcessing(xArray, yArray);
-            for(let i = 0; i < yArray.length; i++){
-                xyArray.push(xArray[i]*yArray[i]);
-            }
-            document.getElementById('lifetime_result').innerHTML = (xyArray.reduce(sum)/yArray.reduce(sum)).toFixed(4);
-            reader.onerror = (error) => {
+                document.getElementById('lifetime_result').innerHTML = (xyArray.reduce(sum)/yArray.reduce(sum)).toFixed(4);
+            }reader.onerror = (error) => {
                 alert("Failed to read file!\n\n" + reader.error);
             };
         reader.readAsText(file1);
